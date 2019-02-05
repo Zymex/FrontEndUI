eb279e77cc455178ec9cd64f7c1ea811be15e2ed
Brandon Workman <fzymez@gmail.com>
2019-02-05 11:16:41 -0600

    FrontEndUI Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DataEntryComponent } from './data-entry/data-entry.component';
+import { ChartsComponent} from './charts/charts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'etch', component: DataEntryComponent},
+ { path: 'charts', component: ChartsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataEntryComponent } from './data-entry/data-entry.component';
+import { ChartsComponent } from './charts/charts.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      DataEntryComponent,
+      ChartsComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      NgbModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
